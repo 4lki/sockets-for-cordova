96e6655f0b7a51d6bed8afc19127c078c3a112c5
Philipp Alker <philipp.alker@citynode.de>
2022-05-17 11:54:44 +0200

    fixed tls

--- src/android/src/de/alki/socketsforcordova/SocketAdapterImpl.java
+++ src/android/src/de/alki/socketsforcordova/SocketAdapterImpl.java
@@ -62,6 +62,7 @@ public class SocketAdapterImpl implements SocketAdapter {
         context.init(null, trustAllCerts, new java.security.SecureRandom());
         SSLSocketFactory factory = context.getSocketFactory();
         socket = (SSLSocket) factory.createSocket();
+        socket.setEnabledProtocols(new String[]{"TLS_v1_2"});
         return socket;
       } catch (IOException | NoSuchAlgorithmException | KeyManagementException e) {
         Logging.Error(SocketAdapterImpl.class.getName(), "Error during connecting of socket", e.getCause());
